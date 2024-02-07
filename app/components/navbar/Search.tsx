7f4e7e3891ec231e340f0139dfd25c219423f9b8
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-02-07 18:19:27 +0000

    n

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -17,9 +17,7 @@ const Search = () => {
                 <div className="text-sm pl-6 pl-2 text-gray-600 flex flex-row items-center gap-3">
                     <div className="hidden sm:block">Add Guests</div>
                     <div className="p-2 bg-rose -500 rounded-full text-white">
-                        <BiSearch size={18}>
-
-                        </BiSearch>
+                        <BiSearch size={18} />
                     </div>
                 </div>
             </div>
