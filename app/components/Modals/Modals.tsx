5f6436791e757a79d6479cf81651cc9511f56eb3
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-02-07 20:33:00 +0000

    s

--- app/components/Modals/Modals.tsx
+++ app/components/Modals/Modals.tsx
@@ -1,5 +1,6 @@
 'use client';
 import {useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 interface ModalProps {
     isOpen?: boolean;
@@ -73,7 +74,86 @@ const Modal: React.FC<ModalProps> = ({
             outline-none
             focus:outline-none
             bg-neutral-800/70">
-
+                <div className="
+                relative
+                w-full
+                md:w-4/6
+                lg:w-3/6
+                xl:w-2/5
+                my-6
+                mx-auto
+                h-full
+                lg:h-auto
+                md:h-auto">
+                    {/* CONTENT */}
+                    <div className={" translate duration-300 h-full " + 
+                    (showModal ? 'translate-y-0' : 'translate-y-full') + 
+                    (showModal ? 'opacity-100' : 'opacity-0')
+                    }>
+                        <div className="
+                        translate
+                        h-full
+                        lg:h-auto
+                        md:h-auto
+                        border-0
+                        rounded-lg
+                        shadow-lg
+                        relative
+                        flex
+                        flex-col
+                        w-col
+                        bg-white
+                        outline-none
+                        focus:outline-none">
+                            {/*HEADER*/}
+                            <div className="
+                            flex
+                            items-center
+                            p-6
+                            rounded-t
+                            justify-center
+                            relative
+                            border-b-[1px]">
+                                <button className="
+                                p-1
+                                border-0
+                                hover:opacity-70
+                                transition
+                                absolute
+                                left-9">
+                                    <IoMdClose size={18} />
+                                </button>
+                                <div className="
+                                text-lg
+                                font-semibold">
+                                    {title}
+                                </div>
+                            </div>
+                            {/*BODY*/}
+                            <div className="
+                            relative
+                            p-6
+                            flex-auto">
+                                {body}
+                            </div>
+                            {/*FOOTER*/}
+                            <div className="
+                            flex
+                            flex-col
+                            gap-2
+                            p-6">
+                                <div className="
+                                flex
+                                flex-row
+                                items-center
+                                gap-4
+                                w-full">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </>
